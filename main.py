533f57a272e489d57294a3a68ed78afdf9fe82bb
Kashvi Jain <kashvipeehu24@gmail.com>
2026-06-10 00:39:34 +0530

    Create a input for user

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+from model import StartupIdea
+
 def show_title():
     print("\n" + "=" * 40)
     print("      AI STARTUP IDEA ANALYZER")
     print("=" * 40)
-
 
 def collect_idea():
     idea_name = input("\nStartup Idea Name: ")
     problem = input("Problem Being Solved: ")
     audience = input("Target Audience: ")
 
-    print("\n" + "=" * 40)
-    print("IDEA SUMMARY")
-    print("=" * 40)
-    print(f"Idea Name      : {idea_name}")
-    print(f"Problem        : {problem}")
-    print(f"Target Audience: {audience}")
-    print("=" * 40)
+    idea = StartupIdea(
+        idea_name,
+        problem,
+        audience
+    )
+
+    idea.display()
 
 
 def main():
